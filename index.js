cd45b336c8bb07ead23c3ac2a09b6e4075e20013
deivsonbf <deivsonb7@gmail.com>
2021-06-13 11:58:26 -0300

    testando

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello, Deivson'))
+app.get('/', (req, res) => res.send('Integração Heroku / GIT'))
 
-app.listen(process.env.PORT, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port port!`))
