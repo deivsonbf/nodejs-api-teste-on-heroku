c61916716b0e8b48aa6198c7952ccd2bc95c81dd
deivsonbf <deivsonb7@gmail.com>
2021-06-13 11:51:48 -0300

    testando

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, Deivson'))
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(process.env.PORT, () => console.log(`Example app listening on port port!`))
